02aa40619dacea4637e9404ce42d7a9d2225bf2f
Naveen Kumar <nk332720@gmail.com>
2024-04-03 21:57:04 +0500

    push my calculator to github

---
+++ dora.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([ 
+    {message:"Enter your first number",type:"number",name:"firstnumber"},              
+    {message: " Enter second number", type:"number", name:"secondnumber" },
+{
+message:"select one of the opreator to perform a operation",
+type:"list",
+name:"operator",
+choices:["addition","subtraction","division","multiplication"],
+},
+]);
+
+// conditional statement
+if (answer.operator === "addition"){
+   console.log ( answer.firstnumber + answer.secondnumber );
+}
+else if (answer.operator === "subtraction"){
+   console.log  ( answer.firstnumber - answer.secondnumber  );
+}
+else if(answer.operator === "division"){
+   console.log ( answer.firstnumber / answer.secondnumber  );
+}
+else if(answer.operator === "multiplication"){
+   console.log ( answer.firstnumber * answer.secondnumber);
+}
+else {
+    console.log("please select valid operator")
+}
